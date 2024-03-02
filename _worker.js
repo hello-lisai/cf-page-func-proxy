f2bd6ab00a101804bc954499b294ac4ec6140992
hello-lisai <105758032+hello-lisai@users.noreply.github.com>
2024-03-01 19:55:42 -0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
+        url.hostname="hi.lisai.link";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
